fd61d630ca7a9dcc38b9787c00c7c7fb89e91e7a
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2024-12-11 17:23:09 -0700

    test(register): use faker to generate unique emails

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,14 +1,23 @@
 import { test, expect } from "playwright-test-coverage";
+import { faker } from "@faker-js/faker";
 
 test("register a new user", async ({ page }) => {
+  const user = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+  };
+  const name = `${user.firstName} ${user.lastName}`;
+  const alias = `${user.firstName[0]}${user.lastName[0]}`;
+  const email = faker.internet.email(user);
+
   await page.goto("http://localhost:5173/");
   await page.getByRole("link", { name: "Register" }).click();
-  await page.getByPlaceholder("Full name").fill("John Doe");
-  await page.getByPlaceholder("Email address").fill("john.doe@example.com");
-  await page.getByPlaceholder("Password").fill("password");
+  await page.getByPlaceholder("Full name").fill(name);
+  await page.getByPlaceholder("Email address").fill(email);
+  await page.getByPlaceholder("Password").fill(faker.internet.password());
   await page.getByRole("button", { name: "Register" }).click();
-  await page.getByRole("link", { name: "JD" }).click();
-  await expect(page.getByRole("main")).toContainText("John Doe");
-  await expect(page.getByRole("main")).toContainText("john.doe@example.com");
+  await page.getByRole("link", { name: alias }).click();
+  await expect(page.getByRole("main")).toContainText(name);
+  await expect(page.getByRole("main")).toContainText(email);
   await page.getByRole("link", { name: "Logout" }).click();
 });
